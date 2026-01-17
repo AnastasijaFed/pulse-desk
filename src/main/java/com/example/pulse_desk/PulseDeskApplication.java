7962b7221b37704f2518ba68ff0a9268b255d493
AnastasijaFed <nastefedorenko@gmail.com>
2026-01-17 19:26:17 +0200

    Initial Spring Boot skeleton

---
+++ src/main/java/com/example/pulse_desk/PulseDeskApplication.java
@@ -0,0 +1,13 @@
+package com.example.pulse_desk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulseDeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pulse_desk/PulseDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pulse_desk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
