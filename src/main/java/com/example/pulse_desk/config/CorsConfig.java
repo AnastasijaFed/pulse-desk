e62558b29fe9028f07acb1f21bdbbe4538a5e3c0
AnastasijaFed <nastefedorenko@gmail.com>
2026-01-20 16:16:29 +0200

    changes for deploying

--- src/main/java/com/example/pulse_desk/config/CorsConfig.java
+++ src/main/java/com/example/pulse_desk/config/CorsConfig.java
@@ -6,13 +6,15 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
+    
     @Bean
     WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("http://localhost:5173")
+                        .allowedOriginPatterns("http://localhost:5173", 
+                            "https://frontend-name.vercel.app")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
